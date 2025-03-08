c513f9ffe1d52325be2a0040a7fdeea8dd9940ac
drdv <mail.mitko@gmail.com>
2025-03-08 17:05:55 +0000

    Deploying to gh-pages from @ drdv/git-dag@fd31d693fecb9adea3aca52fba0ea0f702d8e441 🚀

--- docs/sphinx/src/.templates/README.md
+++
@@ -1,3 +0,0 @@
-# Custom templates for sphinx.ext.autosummary
-
-See https://stackoverflow.com/a/62613202
--- src/git_dag/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Module interface."""
-
-try:
-    from ._version import __version__
-except ModuleNotFoundError:
-    __version__ = "unknown (package not installed)"
-
-from .git_repository import GitRepository
