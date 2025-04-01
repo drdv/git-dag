c078a564c56a79b3e6da3ad5c18448615b4a18a1
drdv <mail.mitko@gmail.com>
2025-04-01 16:13:16 +0000

    Deploying to gh-pages from @ drdv/git-dag@99feb721efc4e790ef7898c9aed6653185fe66eb 🚀

--- _sources/examples.rst.txt
+++ _sources/examples.rst.txt
@@ -3,4 +3,5 @@ Examples
 
 .. toctree::
 
+   example_rebase_onto
    example_git_internals
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+gbc6ab8d',
+    VERSION: '0.1.dev1+g99feb72',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
